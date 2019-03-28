284f3f3e02af4ecd0a1ef37b1ffbf8689bf1154d
Umar Al-Kfaween <omar.ka923@gmail.com>
2019-03-28 12:13:42 +0200

    Generate messages using core method

--- lib/errors_full_details/core_ext.rb
+++ lib/errors_full_details/core_ext.rb
@@ -2,9 +2,8 @@ module ActiveModel
   class Errors
     def full_details
       details.each_with_object([]) do |(attr, error_arr), errors|
-        attr_name = attr.to_s.humanize
         error_arr.each do |error|
-          message = (attr == :base ? '' : "#{attr_name} ") + error[:error]
+          message = full_message(attr, error[:error])
           code = error.key?(:code) ? error[:code].to_s : '0'
           errors << { message: message, code: code }
         end
